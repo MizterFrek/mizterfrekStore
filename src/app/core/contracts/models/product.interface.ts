8541f9a086afcd096b8409f647eddc179af55bb0
MizterFrek© <jesus.llica@makingconnexion.com>
2024-05-20 20:16:27 -0500

    Agregando modelo Producto del servicio API

---
+++ src/app/core/contracts/models/product.interface.ts
@@ -0,0 +1,11 @@
+import { Rating } from "./rating.interface";
+
+export interface Produc {
+    id:          number;
+    title:       string;
+    price:       number;
+    description: string;
+    category:    string;
+    image:       string;
+    rating:      Rating;
+}
