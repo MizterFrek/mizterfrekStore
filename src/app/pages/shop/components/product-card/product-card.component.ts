64e22762a9f5b2916b19973fb31599148880dd53
MizterFrek© <jesus.llica@makingconnexion.com>
2024-05-20 20:17:39 -0500

    Creación de Product-card-component

---
+++ src/app/pages/shop/components/product-card/product-card.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Produc } from '../../../../core/contracts/models/product.interface';
+import { StarRatingComponent } from '../../../../shared/components/star-rating/star-rating.component';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [
+    CommonModule,
+    CurrencyPipe,
+    StarRatingComponent
+  ],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.scss',
+})
+export class ProductCardComponent {
+  @Input({required: true}) product!: Produc;
+}
