e3fbff0f50a732693cb44c0c7b8a7ab446fbbb4c
MizterFrek© <jesus.llica@makingconnexion.com>
2024-05-20 20:52:22 -0500

    Agregando Sort Overlay

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ProductCardComponent } from './components/product-card/product-card.component';
 import { StoreApiService } from '../../core/services/api/store-api.service';
 import { tap } from 'rxjs';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
   imports: [
     CommonModule,
-    ProductCardComponent
+    ProductCardComponent,
+    OverlayModule
   ],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss',
@@ -19,6 +21,7 @@ export class ShopComponent {
   private api = inject(StoreApiService);
 
   public loading = signal<boolean>(true);
+  public openSort = false;
 
   products = this.api.products().pipe(
     tap(() => this.loading.set(false))
