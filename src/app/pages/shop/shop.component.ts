45aa913d0160b64f9b9df8aca93f8bc9bcc0cf5f
MizterFrek© <jesus.llica@makingconnexion.com>
2024-05-20 20:17:24 -0500

    Consumiendo servicio en ShopComponent

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -1,13 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { StoreApiService } from '../../core/services/api/store-api.service';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
   imports: [
     CommonModule,
+    ProductCardComponent
   ],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss',
 })
-export class ShopComponent { }
+export class ShopComponent {
+
+  private api = inject(StoreApiService);
+
+  public loading = signal<boolean>(true);
+
+  products = this.api.products().pipe(
+    tap(() => this.loading.set(false))
+  );
+
+}
